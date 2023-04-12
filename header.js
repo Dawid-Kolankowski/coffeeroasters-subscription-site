025fce619212f882c7a8b9826dd059d54c53fc12
Dawid Kolankowski <nexog1997@gmail.com>
2023-04-12 13:04:13 +0200

    add nav bar, no mobile

---
+++ header.js
@@ -0,0 +1,15 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector(".header");
+const body = document.body;
+const overlay = document.querySelector(".overlay");
+
+btnHamburger.addEventListener("click", function () {
+  console.log(overlay);
+  header.classList.toggle("open");
+  body.classList.toggle("no-scroll");
+});
+
+overlay.addEventListener("click", function () {
+  header.classList.toggle("open");
+  body.classList.toggle("no-scroll");
+});
